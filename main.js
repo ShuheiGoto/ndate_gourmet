1d420c0960a7b6ac417fa80755c8b9e48ea84f55
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-06-21 17:38:51 +0900

    popup.jsとmain.jsの追記

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ console.log("background page");
 
 var desiredHour = "0";
 var desireMinute = "0";
+var desireLocation = "";
+var desireGenre = "";
 
 chrome.alarms.onAlarm.addListener(function(alarm){
     console.log("Alarm fired!");
@@ -10,4 +12,49 @@ chrome.alarms.onAlarm.addListener(function(alarm){
     tabProperties = { url: "https://www.youtube.com" };
     chrome.tabs.create(tabProperties);
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    if (request.setValues == false) {
+        sendResponse({
+            hour: desiredHour,
+            minite: desireMinute,
+        });
+        return;
+    }
+
+    dateToFire = new Date();
+    desireLocation = request.location;
+    desireGenre = request.genre;
+
+    chrome.alarms.clearAll();
+    timeArray = request.time.split(":");
+    if(timeArray.length != 2) {
+        sendResponse({ error: "Invalid Time"});
+        return;
+    }
+
+    desiredHour = timeArray[0];
+    desireMinute = timeArray[1];
+    dateToFire.setHours((desiredHour));
+    dateToFire.setMinutes(desireMinute);
+    dateToFire.setSeconds(0);
+
+    timeToFire = dateToFire.getTime();
+    if(timeToFire < Date.now()) {
+        // 現在より前の自国の場合には明日向けにセットする
+        console.log("setting time for today...");
+        timeToFire += 1000 * 60 * 60 * 24;
+    } else {
+        console.log("Setting time for today...");
+    }
+    console.log("Time to fire: " + timeToFire);
+
+    alarmPeriod = 60 * 24;
+    alarmInfo = { when: timeToFire, periodInMinutes: alarmPeriod };
+    alarmA = chrome.alarms.create("alarm", alarmInfo);
+
+    sendResponse({});
+});
+
 
--- popup.js
+++ popup.js
@@ -4,9 +4,38 @@ function sendFromToBackground(submittedForm){
         time: submittedForm['time'].value,
         location: submittedForm['location'].value,
         genre: submittedForm['genre'].value
-    }
-    chrome.runtime.sendMessage(msg, function(response){
+    };
 
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        console.log(tabs);
+        console.log(tabs[0].url);
+        chrome.tabs.sendMessage(tabs[0].id, msg, function(response) {
+            if (response.error == undefined) {
+                document.getElementById("resultText").style.color='#00EE18';
+                document.getElementById("resultText").innerHTML='Alarm Set';
+            } else {
+                document.getElementById("resultText").style.color='#E02000';
+                document.getElementById("resultText").innerHTML='Alarm Not Set: ' + response.error;
+            }
+        });
+    });
+    /*chrome.runtime.sendMessage(msg, function(response){
+        if (response.error == undefined) {
+            document.getElementById("resultText").style.color='#00EE18';
+            document.getElementById("resultText").innerHTML='Alarm Set';
+        } else {
+            document.getElementById("resultText").style.color='#E02000';
+            document.getElementById("resultText").innerHTML='Alarm Not Set: ' + response.error;
+        }
+    });
+    */
+}
+
+function getCurrentAlarm(formToSubmit) {
+    chrome.runtime.sendMessage({ setValues: false}, function (response) {
+        formToSubmit['time'].value = response.time;
+        formToSubmit['location'].value = response.location;
+        formToSubmit['genre'].value = response.genre;
     });
 }
 
