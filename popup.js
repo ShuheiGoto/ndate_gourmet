f715588861a25135bc225f209aa7a2cb826a6460
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-06-21 17:59:48 +0900

    やっぱbackground.jsでalarmを実行する

--- popup.js
+++ popup.js
@@ -5,21 +5,7 @@ function sendFromToBackground(submittedForm){
         location: submittedForm['location'].value,
         genre: submittedForm['genre'].value
     };
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        console.log(tabs);
-        console.log(tabs[0].url);
-        chrome.tabs.sendMessage(tabs[0].id, msg, function(response) {
-            if (response.error == undefined) {
-                document.getElementById("resultText").style.color='#00EE18';
-                document.getElementById("resultText").innerHTML='Alarm Set';
-            } else {
-                document.getElementById("resultText").style.color='#E02000';
-                document.getElementById("resultText").innerHTML='Alarm Not Set: ' + response.error;
-            }
-        });
-    });
-    /*chrome.runtime.sendMessage(msg, function(response){
+    chrome.runtime.sendMessage(msg, function(response){
         if (response.error == undefined) {
             document.getElementById("resultText").style.color='#00EE18';
             document.getElementById("resultText").innerHTML='Alarm Set';
@@ -28,7 +14,6 @@ function sendFromToBackground(submittedForm){
             document.getElementById("resultText").innerHTML='Alarm Not Set: ' + response.error;
         }
     });
-    */
 }
 
 function getCurrentAlarm(formToSubmit) {
